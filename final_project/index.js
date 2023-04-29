3b1ddacbb9bee43c6994dd16a4088277366f21a6
ronson_danthy <ronson.mitmpl2022@learner.manipal.edu>
2023-04-29 14:13:55 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,26 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+  // extract username and password from the request body
+  const { username, password } = req.body;
+
+  // check if the username and password are valid
+  if (isValidUser(username, password)) {
+    // generate a JWT token with a secret key
+    const token = jwt.sign({ username }, 'secret_key');
+    
+    // attach the token to the response header
+    res.set('Authorization', `Bearer ${token}`);
+    
+    // proceed to the next middleware
+    next();
+  } else {
+    // return an error response if the credentials are invalid
+    res.status(401).send('Invalid username or password');
+  }
+
+
 });
  
 const PORT =5000;
